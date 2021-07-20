406eb7939102008ec4c4403869d7ab5260b94e55
sb <stephane.baudry.vanaa@gmail.com>
2021-07-20 12:43:32 +0200

    final basic version

---
+++ web-next-styled-101/components/Footer.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const FooterSection = styled.div`
+  background: #000;
+  color: #fff;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function Footer() {
+  return (
+    <>
+      <FooterSection>
+        <p>Next 2021 All Rights Reserved</p>
+      </FooterSection>
+    </>
+  );
+}
+
+export default Footer;
--- web-next-styled-101/components/Navbar.js
+++ web-next-styled-101/components/Navbar.js
@@ -1,8 +1,39 @@
+import styled from "styled-components";
+import Link from "next/Link";
+
+const Nav = styled.nav`
+  height: 80px;
+  background: #1a0aa8;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledLink = styled.a`
+  padding: 0rem 2rem;
+`;
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Nav>
+      <div>
+        <Link href="/" passHref>
+          <StyledLink>NXT</StyledLink>
+        </Link>
+      </div>
+      <div>
+        <Link href="/" passHref>
+          <StyledLink>Home</StyledLink>
+        </Link>
+        <Link href="/about" passHref>
+          <StyledLink>About</StyledLink>
+        </Link>
+        <Link href="/contact" passHref>
+          <StyledLink>Contact</StyledLink>
+        </Link>
+      </div>
+    </Nav>
   );
 };
 
